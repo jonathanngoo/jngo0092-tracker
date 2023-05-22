7e06f2b3251457f002398b9b6dadfb12ff78c12b
Jonathan Ngo <jonathannn1.ngo@hotmail.com>
2023-05-22 23:16:20 +1000

    Finalised website and started mobile.

--- public/script.js
+++ public/script.js
@@ -1,26 +1,35 @@
-// Adding image and displaying on form from https://talkerscode.com/howto/upload-image-in-html-and-display.php#:~:text='image'%20variable%20containing%20uploaded%20image,the%20end%20of%20HTML%20document. 
-var loadFile = function(event) {
+// Adding image and displaying on form from https://talkerscode.com/howto/upload-image-in-html-and-display.php#:~:text='image'%20variable%20containing%20uploaded%20image,the%20end%20of%20HTML%20document.
+var loadFile = function (event) {
   var image = document.getElementById("output");
-  image.src=URL.createObjectURL(event.target.files[0]);
+  image.src = URL.createObjectURL(event.target.files[0]);
 };
 
 // Displaying data from form individually https://www.youtube.com/watch?v=f3fALMAVBOE
-document.querySelector(document).ready (function(){
-  document.querySelector("#name").on("change", function(){
-    document.querySelector("data").hide ();
-  }).addEventListener("change",);
+var content= document.getElementById("content");
+document.addEventListener("DOMContentLoaded",function () {
+  document
+    .querySelector("#name")
+    .querySelector("change", function () {
+      document.querySelector("data").hide();
+    })
+    .addEventListener("change");
 });
 
 // Generating today's date from server https://stackoverflow.com/questions/71953018/html-input-date-field-how-to-set-default-value-to-todays-date
 function getDate() {
   var today = new Date();
 
-  document.getElementById("date").value = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2) + "-" + ("0" + today.getDate()).slice(-2);
+  document.getElementById("date").value =
+    today.getFullYear() +
+    "-" +
+    ("0" + (today.getMonth() + 1)).slice(-2) +
+    "-" +
+    ("0" + today.getDate()).slice(-2);
 }
 
 // Adding a rating system when the user types their movie name https://codepen.io/neilpomerleau/pen/wzxzQM
-$(":radio").change(function() {
-  console.log("New star rating: " + this.value);
+document.querySelector('radio').addEventListener('change',function () {
+  console.log('New star rating: ' + this.value);
 });
 
 const form = document.getElementById("taskform");
@@ -28,14 +37,18 @@ const tasklistElem = document.querySelector("#tasklist");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(form.elements.taskName.value);
+  console.log(form.elements.UserFirstName.value);
   addTask(
-    form.elements.taskName.value,
-    form.elements.taskType.value,
-    form.elements.taskRate.value,
-    form.elements.taskTime.value,
-    form.elements.taskClient.value,
-    form.elements.taskLocation.value
+    form.elements.UserFirstName.value,
+    form.elements.UserLastName.value,
+    form.elements.UserId.value,
+    form.elements.DateSubmitted.value,
+    form.elements.MovieGenre.value,
+    form.elements.phoneNumber.value,
+    form.elements.Email.value,
+    form.elements.movieName.value,
+    form.elements.ratingTitle.value,
+    form.elements.movieImage.value
   );
   console.log(taskList);
 });
@@ -44,13 +57,13 @@ function displayTask(task) {
   let item = document.createElement("li");
   item.setAttribute("data-id", task.id);
   item.innerHTML = `<p> 
-  <strong>${task.name}</strong><br>${task.type} <br>${task.rate}<br>${task.time}<br>${task.client}
-  <br>${task.location}<p>`;
+  <strong>${user.firstname}</strong><br>${user.lastname} <br>${user.id}<br>${date.submitted}<br>${movie.genre} 
+  <br>${phone.number}<br>${email}<br>${movie.name}<br>${rating.title}<br>${movie.image}<p>`;
   tasklistElem.appendChild(item);
   form.reset();
 
   let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete");
+  let delButtonText = document.createTextNode("🗑️");
   delButton.appendChild(delButtonText);
   item.appendChild(delButton);
 
@@ -66,8 +79,8 @@ function displayTask(task) {
 }
 
 function displayRadioValue() {
-  var value = document.getElementsByName("taskLocation").value;
-  document.getElementById("taskLocation").innerHTML = value;
+  var value = document.getElementsByName("UserFirstName").value;
+  document.getElementById("UserFirstName").innerHTML = value;
 }
 
 // Create an object called 'task'
@@ -96,7 +109,8 @@ var taskList = [];
 // Replace the property values with the input paramaters
 // Add the object to the taskList array
 
-function addTask(name, type, rate, time, client, location) {
+function addTask(userfirstname, userlastname, userid, datesubmitted, moviegenre, phonenumber
+  ,email, moviename, rating, movieimage) {
   // Creating the object with the usual property:value syntax
   // Create task object
   // let task = {
@@ -111,19 +125,20 @@ function addTask(name, type, rate, time, client, location) {
 
   // Creating the object, directly passing in the input parameters
   let task = {
-    name,
-    type,
-    id: Date.now(),
-    date: new Date().toISOString(),
-    rate,
-    time,
-    client,
-    location,
+    userfirstname,
+    userlastname,
+    userid,
+    datesubmitted: Date.now(),
+    moviegenre,
+    phonenumber,
+    email,
+    moviename,
+    rating,
+    movieimage,
   };
 
   taskList.push(task);
   displayTask(task);
-
 
   let listButton = document.createElement("button");
   let card = document.createElement("card");
@@ -131,7 +146,7 @@ function addTask(name, type, rate, time, client, location) {
   card.appendChild(listButton);
 
   listButton.addEventListener("click", function (event) {
-    console.log(country.name.common);
+    console.log(user.name.common);
 
     let listOftasks = JSON.parse(localStorage.getItem("listOftasks"));
 
@@ -141,12 +156,12 @@ function addTask(name, type, rate, time, client, location) {
     } else {
       if (
         listOftasks.find(
-          (element) => element.name.common === client.name.common
+          (element) => element.user.name.common === user.last.name.common
         )
       ) {
         console.log("Person already exists");
       } else {
-        listOftasks.push(client);
+        listOftasks.push(user);
       }
     }
 
@@ -162,7 +177,7 @@ function updateList() {
 }
 
 // Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 35, 5, "Google", "Sydney");
+// addTask("Initial Sketches", "Concept Ideation", 35, 5, "Google", "Sydney");
 
 // Log the array to the console.
 console.log(taskList);
